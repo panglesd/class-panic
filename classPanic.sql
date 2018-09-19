380dbd84cc85c8e1e5a75c61bac08bcdd0c9890c
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2018-09-19 14:51:40 +0200

    ajout des modifs bdd à classPanic.sql

--- classPanic.sql
+++ classPanic.sql
@@ -1,158 +1,252 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb2+deb7u8
--- http://www.phpmyadmin.net
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 25, 2018 at 01:00 PM
--- Server version: 5.5.60
--- PHP Version: 5.4.45-0+deb7u14
+-- Hôte : localhost
+-- Généré le :  mer. 19 sep. 2018 à 12:49
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.10
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `classPanic`
+-- Base de données :  `Projet`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `poll`
+-- Structure de la table `poll`
 --
 
-CREATE TABLE IF NOT EXISTS `poll` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `poll` (
+  `id` int(11) NOT NULL,
   `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `response` int(11) NOT NULL,
   `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `roomID` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `OneByRoom` (`pseudo`,`roomID`),
-  KEY `pseudo` (`pseudo`),
-  KEY `roomID` (`roomID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `questions`
---
-
-CREATE TABLE IF NOT EXISTS `questions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `enonce` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `indexSet` int(11) NOT NULL,
-  `class` int(11) NOT NULL,
-  `owner` int(11) NOT NULL,
-  `reponses` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `correct` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `appartenance class` (`class`),
-  KEY `ownerID` (`owner`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=32 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rooms`
---
-
-CREATE TABLE IF NOT EXISTS `rooms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_currentQuestion` int(11) NOT NULL,
-  `questionSet` int(11) NOT NULL,
-  `ownerID` int(11) NOT NULL,
-  `status` enum('pending','revealed') COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `rooms_ibfk_1` (`id_currentQuestion`),
-  KEY `ownerID` (`ownerID`),
-  KEY `questionSet` (`questionSet`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=269 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sessions`
---
-
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `expires` int(11) unsigned NOT NULL,
-  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-  PRIMARY KEY (`session_id`)
+  `roomID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `setDeQuestion`
+-- Structure de la table `questions`
 --
 
-CREATE TABLE IF NOT EXISTS `setDeQuestion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `enonce` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `indexSet` int(11) NOT NULL,
+  `class` int(11) NOT NULL,
   `owner` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name and owner` (`name`,`owner`) USING BTREE,
-  KEY `idOwner` (`owner`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=12 ;
+  `reponses` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `correct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `rooms`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rooms` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_currentQuestion` int(11) NOT NULL,
+  `questionSet` int(11) NOT NULL,
+  `ownerID` int(50) NOT NULL,
+  `status` enum('pending','revealed') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `question` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `setDeQuestion`
+--
+
+CREATE TABLE `setDeQuestion` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stats`
+--
+
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL,
+  `userID` int(11) NOT NULL,
+  `roomName` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `setName` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `correct` enum('juste','faux','NSPP') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `questionType` enum('custom','set') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'set'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(110) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
   `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
-  `fullName` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=20 ;
+  `fullName` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Constraints for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Constraints for table `poll`
+-- Index pour la table `poll`
 --
 ALTER TABLE `poll`
-  ADD CONSTRAINT `poll_ibfk_4` FOREIGN KEY (`roomID`) REFERENCES `rooms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `poll_ibfk_3` FOREIGN KEY (`pseudo`) REFERENCES `users` (`pseudo`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pseudo and room` (`pseudo`,`roomID`);
 
 --
--- Constraints for table `questions`
+-- Index pour la table `questions`
 --
 ALTER TABLE `questions`
-  ADD CONSTRAINT `questions_ibfk_3` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `questions_ibfk_2` FOREIGN KEY (`class`) REFERENCES `setDeQuestion` (`id`) ON DELETE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `appartenance class` (`class`),
+  ADD KEY `owner` (`owner`);
 
 --
--- Constraints for table `rooms`
+-- Index pour la table `rooms`
 --
 ALTER TABLE `rooms`
-  ADD CONSTRAINT `rooms_ibfk_8` FOREIGN KEY (`questionSet`) REFERENCES `setDeQuestion` (`id`),
-  ADD CONSTRAINT `rooms_ibfk_6` FOREIGN KEY (`ownerID`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `rooms_ibfk_7` FOREIGN KEY (`id_currentQuestion`) REFERENCES `questions` (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `rooms_ibfk_1` (`id_currentQuestion`),
+  ADD KEY `ownerID` (`ownerID`);
 
 --
--- Constraints for table `setDeQuestion`
+-- Index pour la table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+--
+-- Index pour la table `setDeQuestion`
 --
 ALTER TABLE `setDeQuestion`
-  ADD CONSTRAINT `setDeQuestion_ibfk_2` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name and owner` (`name`,`owner`) USING BTREE,
+  ADD KEY `idOwner` (`owner`);
+
+--
+-- Index pour la table `stats`
+--
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pseudo` (`pseudo`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `poll`
+--
+ALTER TABLE `poll`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+
+--
+-- AUTO_INCREMENT pour la table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `setDeQuestion`
+--
+ALTER TABLE `setDeQuestion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT pour la table `stats`
+--
+ALTER TABLE `stats`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `poll`
+--
+ALTER TABLE `poll`
+  ADD CONSTRAINT `poll_ibfk_1` FOREIGN KEY (`pseudo`) REFERENCES `users` (`pseudo`);
+
+--
+-- Contraintes pour la table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `appartenance class` FOREIGN KEY (`class`) REFERENCES `setDeQuestion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `owner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`id_currentQuestion`) REFERENCES `questions` (`id`),
+  ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`ownerID`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `setDeQuestion`
+--
+ALTER TABLE `setDeQuestion`
+  ADD CONSTRAINT `idOwner` FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
