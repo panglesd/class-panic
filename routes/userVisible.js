1f5a05b5a58a3ab7db14cfbfca7b97efacf1a756
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2018-09-29 13:24:50 +0200

    mini modif routes

--- routes/userVisible.js
+++ routes/userVisible.js
@@ -40,8 +40,11 @@ router.use(function (req, res, next) {
 router.get('/room', room_controller.room_list);
 
 
-// GET request for showing courses list.
+// GET request for showing subscribed courses list.
 router.get('/course', course_controller.courses_list);
+
+// GET request for subscribing to a courses.
+//router.get('/subscribe', course_controller.courses_subscribe);
 
 // GET request for showing a course.
 //router.get('/course/:id', course_controller.course);
