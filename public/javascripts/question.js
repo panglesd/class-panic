998266d04cbdd34740b0aef5d7f60e61dceaad45
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2018-10-21 14:56:26 +0200

    Bien meilleure html/js pour manage_question

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -1,19 +1,115 @@
-function toggleText(elem) {
-    elem.classList.toggle("texted");
-    value = elem.querySelector("input").value = elem.querySelector("input").value=="true" ? "false" : "true";
-    elem.nextSibling.style.visibility = elem.nextSibling.style.visibility=="hidden" /* && elem.parentNode.classList.contains("true")*/ ? "visible" : "hidden";
-    text = elem.querySelector("span")
-    if(text.textContent == "Enlever le champs texte")
-	text.textContent = "Ajouter un champs texte";
-    else
-	text.textContent = "Enlever le champs texte";
-}
+// Le HTML d'une question
 
-function returnTextQuestion (i) {
-    return '<div style="text-align:left;margin:3px;" onclick="toggleText(this)"><input type="hidden" name="text-'+i+'" value="false"><span>Ajouter un champs texte</span></div><textarea name="correction-'+i+'" style="visibility:hidden; display:block; width:100%" placeholder="Vous pouvez rentrer la correction/justification/explication" ></textarea>'
+function returnHTMLQuestion (i) {
+    return '	<!--    Regroupement des champs hidden   -->'+
+'		<input type="hidden" class="texted-hidden" name="texted-'+i+'" value="false">'+
+'		<input type="hidden" class="correctness-hidden" name="correctness-'+i+'" value="false">'+
+'	<!--    Numéro de réponse                -->'+
+'		<span class="froom nreponse"> Réponse '+i+' :</span>'+
+'	<!--    Texte de la réponse              -->'+
+'		<textarea'+
+'		    placeholder="Ne sera pas interprété par markdown. Pour les maths, utiliser \(...\) ou \[...\]"'+
+'		    class="value-reponse-textarea" name="value-reponse-'+i+'"'+
+'		    style="width:100%"></textarea>'+
+'	<!--    Réponse custom ou non            -->'+
+'		<div'+
+'		    class="text"'+
+'		    style="text-align:left;margin:3px;">'+
+'		    <span>Ajouter un champs texte</span>'+
+'		</div>'+
+'		<textarea'+
+'		    class="correction-textarea"'+
+'		    name="correction-'+i+'"'+
+'		    style="visibility:hidden;display:block; width:100%"'+
+'		    placeholder="Vous pouvez rentrer la correction/justification/explication" ></textarea>'+
+'	<!--    Boutons pour le management       -->'+
+'		    <div>'+
+'			<ul>'+
+'			    <li style="display:inline-block; margin-right:20px;">'+
+'				<a class="room remove">Enlever cette réponse</a>'+
+'			    </li>'+
+'			    <li class="set-true select" >'+
+'				Réponse juste'+
+'			    </li>'+
+'			    <li class="set-to_correct select" >'+
+'				Réponse à corriger'+
+'			    </li>'+
+'			    <li class="set-false select selected" >'+
+'				Réponse fausse'+
+'			    </li>'+
+'			</ul>'+
+'		    </div>'+
+'';
 }
 
 function addAnswer () {
+    ul = document.querySelector("#listeQue");
+    li = document.createElement("li");
+    liButton = document.querySelector("#ajoutrep");
+    n = document.querySelectorAll(".reponse").length;
+    li.innerHTML=returnHTMLQuestion(n);
+    li.classList.add("reponse", "false")
+    ul.insertBefore(li,liButton);
+    addReponseListener(li);
+}
+
+
+// Avoir ou non un champs texte dans la réponse
+
+function toggleText(elem) {
+    text = elem.querySelector(".text");
+    text.classList.toggle("texted");
+    value = elem.querySelector(".texted-hidden").value = elem.querySelector(".texted-hidden").value=="true" ? "false" : "true";
+    elem.querySelector(".correction-textarea").style.visibility = elem.querySelector(".correction-textarea").style.visibility=="hidden" ? "visible" : "hidden";
+    text_string = text.querySelector("span")
+    if(text_string.textContent == "Enlever le champs texte")
+	text_string.textContent = "Ajouter un champs texte";
+    else
+	text_string.textContent = "Enlever le champs texte";
+}
+
+// Régler si une réponse est correcte, fausse ou à corriger
+
+function setValue(value, elem) {
+    elem.classList.remove("true", "false", "to_correct");
+    elem.classList.add(value);
+    elem.querySelector(".correctness-hidden").value = value;
+    elem.querySelector(".select.selected").classList.remove("selected");
+    elem.querySelector(".set-"+value).classList.add("selected");
+}
+
+// Pour la numérotation des réponses et des names
+
+function renumber() {
+    i=0;
+    q = document.querySelectorAll(".reponse");
+    q.forEach(function (elem) {  
+	elem.querySelector(".nreponse").innerHTML = "Réponse "+ i + " :";
+	elem.querySelector(".value-reponse-textarea").name="value-reponse-"+i;
+	elem.querySelector(".texted-hidden").name="texted-"+i;
+	elem.querySelector(".correctness-hidden").name="correctness-"+i;
+	elem.querySelector(".correction-textarea").name="correction-"+i;
+	i++;
+    });
+}
+
+function removeElement(elem) {
+    elem.remove();
+    renumber();
+}
+
+/*************************************************/
+/*    OLD                                     ****/
+/*************************************************/
+
+
+
+
+function returnTextQuestionOLD (i) {
+    return '<div style="text-align:left;margin:3px;" onclick="toggleText(this)"><input type="hidden" name="text-'+i+'" value="false"><span>Ajouter un champs texte</span></div><textarea name="correction-'+i+'" style="visibility:hidden; display:block; width:100%" placeholder="Vous pouvez rentrer la correction/justification/explication" ></textarea>'
+}
+
+function addAnswerOLD () {
     a=document.querySelector("#listeQue");
     b = document.createElement("li");
     c=document.querySelector("#ajoutrep");
@@ -25,8 +121,8 @@ function addAnswer () {
     a.insertBefore(b,c);
 }
 
-function setValue(value, elem) {
-    isMultiAnswer = document.querySelector("#multi").checked;
+function setValueOLD(value, elem) {
+/*    isMultiAnswer = document.querySelector("#multi").checked;
     if (!isMultiAnswer) {
 	let a=document.querySelector(".true");
 	if(a) {
@@ -34,7 +130,7 @@ function setValue(value, elem) {
 	    a.querySelector(".set-true").classList.remove("selected");
 	    a.querySelector(".set-false").classList.add("selected");
 	}
-    }
+    }*/
     elem.classList.remove("true", "false", "to_correct");
     elem.classList.add(value);
     elem.querySelector(".select.selected").classList.remove("selected");
@@ -43,23 +139,19 @@ function setValue(value, elem) {
 //    document.querySelector("#true").value=document.querySelector(".true textarea").id;
 }
 
-function renumber() {
+function renumberOLD() {
     i=0;
-    q = document.querySelectorAll(".nreponse");
+    q = document.querySelectorAll(".reponse");
     q.forEach(function (elem) {  
-	elem.previousElementSibling.innerHTML = "Réponse "+ i + " :";
-	elem.name=""+i;
-	elem.id=""+i;
+	elem.querySelector(".nreponse").innerHTML = "Réponse "+ i + " :";
+	elem.querySelector("value-reponse").name=""+i;
+	elem.querySelector("value-reponse").id=""+i;
 	i++;
     });
-    document.querySelector("#true").value=document.querySelector(".true textarea").id;
-}
-function removeElement(elem) {
-    elem.parentNode.parentNode.parentNode.parentNode.remove();
-    renumber();
+//    document.querySelector("#true").value=document.querySelector(".true textarea").id;
 }
 
-function toggleMultiAnswer() {
+function toggleMultiAnswerOLD() {
     isMultiAnswer = document.querySelector("#multi").checked;
     if(isMultiAnswer) {
 	document.querySelectorAll(".correct").forEach((button) => {
