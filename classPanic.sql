3894d291b4d8c99029b91a3ae72077d1223bfebb
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2019-03-04 15:21:01 +0100

    DB file was outdated

--- classPanic.sql
+++ classPanic.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  jeu. 25 oct. 2018 à 08:13
--- Version du serveur :  10.1.36-MariaDB
--- Version de PHP :  7.2.10
+-- Généré le :  lun. 04 mars 2019 à 14:20
+-- Version du serveur :  10.3.12-MariaDB
+-- Version de PHP :  7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -35,10 +35,43 @@ CREATE TABLE `courses` (
   `commentaire` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Déchargement des données de la table `courses`
+-- Structure de la table `documents`
 --
 
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `courseID` int(11) NOT NULL,
+  `ownerID` int(11) NOT NULL,
+  `filesInfo` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Doublure de structure pour la vue `flatStats`
+-- (Voir ci-dessous la vue réelle)
+--
+CREATE TABLE `flatStats` (
+`time` timestamp
+,`strategy` enum('manual','all_or_0','QCM','computed')
+,`statsID` int(11)
+,`setID` int(11)
+,`setText` text
+,`roomID` int(11)
+,`roomText` text
+,`questionID` int(11)
+,`questionText` text
+,`courseID` int(11)
+,`courseText` text
+,`blocID` int(11)
+,`customQuestion` text
+,`userID` int(11)
+,`correct` text
+,`response` text
+);
 
 -- --------------------------------------------------------
 
@@ -51,14 +84,9 @@ CREATE TABLE `poll` (
   `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `response` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `responseText` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_activity` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `roomID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `poll`
---
-
 
 -- --------------------------------------------------------
 
@@ -74,13 +102,10 @@ CREATE TABLE `questions` (
   `owner` int(11) NOT NULL,
   `reponses` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` enum('mono','multi') COLLATE utf8mb4_unicode_ci NOT NULL
+  `type` enum('mono','multi') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `strategy` enum('manual','all_or_0','QCM') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `coef` int(11) NOT NULL DEFAULT 5
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `questions`
---
-
 
 -- --------------------------------------------------------
 
@@ -91,18 +116,14 @@ CREATE TABLE `questions` (
 CREATE TABLE `rooms` (
   `id` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_currentQuestion` int(11) NOT NULL,
+  `id_currentQuestion` int(11) DEFAULT NULL,
   `questionSet` int(11) NOT NULL,
   `ownerID` int(50) NOT NULL,
-  `status` enum('pending','revealed') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `question` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `courseID` int(11) NOT NULL
+  `courseID` int(11) NOT NULL,
+  `type` enum('live','cc') COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `rooms`
---
-
 
 -- --------------------------------------------------------
 
@@ -113,13 +134,8 @@ CREATE TABLE `rooms` (
 CREATE TABLE `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `expires` int(11) UNSIGNED NOT NULL,
-  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `sessions`
---
-
 
 -- --------------------------------------------------------
 
@@ -134,11 +150,6 @@ CREATE TABLE `setDeQuestion` (
   `courseID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `setDeQuestion`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -148,15 +159,13 @@ CREATE TABLE `setDeQuestion` (
 CREATE TABLE `stats` (
   `id` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
-  `correct` enum('juste','faux','NSPP','unknown') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `blocID` int(11) NOT NULL,
-  `response` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `correct` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `blocID` int(11) DEFAULT NULL,
+  `response` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `strategy` enum('manual','all_or_0','QCM','computed') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customQuestion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `time` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `stats`
---
-
 
 -- --------------------------------------------------------
 
@@ -166,22 +175,15 @@ CREATE TABLE `stats` (
 
 CREATE TABLE `statsBloc` (
   `id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `setID` int(11) NOT NULL,
   `setText` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `roomID` int(11) NOT NULL,
   `roomText` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `questionID` int(11) DEFAULT NULL,
-  `questionText` text COLLATE utf8mb4_unicode_ci,
+  `questionText` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `courseID` int(11) NOT NULL,
-  `courseText` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `customQuestion` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `courseText` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `statsBloc`
---
-
 
 -- --------------------------------------------------------
 
@@ -203,13 +205,9 @@ CREATE TABLE `subscription` (
   `canOwnRoom` tinyint(1) NOT NULL,
   `canAllRoom` tinyint(1) NOT NULL,
   `canOwnSet` tinyint(1) NOT NULL,
-  `canAllSet` tinyint(1) NOT NULL
+  `canAllSet` tinyint(1) NOT NULL,
+  `canAddDocs` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `subscription`
---
-
 
 -- --------------------------------------------------------
 
@@ -222,17 +220,21 @@ CREATE TABLE `users` (
   `pseudo` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(110) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
+  `isAdmin` tinyint(1) NOT NULL DEFAULT 0,
   `fullName` varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
   `promotion` int(11) NOT NULL,
   `studentNumber` int(11) NOT NULL,
   `institution` enum('IUT','FAC') COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Déchargement des données de la table `users`
---
+-- --------------------------------------------------------
 
+--
+-- Structure de la vue `flatStats`
+--
+DROP TABLE IF EXISTS `flatStats`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `flatStats`  AS  select `stats`.`time` AS `time`,`stats`.`strategy` AS `strategy`,`stats`.`id` AS `statsID`,`statsBloc`.`setID` AS `setID`,`statsBloc`.`setText` AS `setText`,`statsBloc`.`roomID` AS `roomID`,`statsBloc`.`roomText` AS `roomText`,`statsBloc`.`questionID` AS `questionID`,`statsBloc`.`questionText` AS `questionText`,`statsBloc`.`courseID` AS `courseID`,`statsBloc`.`courseText` AS `courseText`,`stats`.`blocID` AS `blocID`,`stats`.`customQuestion` AS `customQuestion`,`stats`.`userID` AS `userID`,`stats`.`correct` AS `correct`,`stats`.`response` AS `response` from (`statsBloc` join `stats` on(`statsBloc`.`id` = `stats`.`blocID`)) ;
 
 --
 -- Index pour les tables déchargées
@@ -242,6 +244,12 @@ CREATE TABLE `users` (
 -- Index pour la table `courses`
 --
 ALTER TABLE `courses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `documents`
+--
+ALTER TABLE `documents`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -265,9 +273,9 @@ ALTER TABLE `questions`
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`),
-  ADD KEY `rooms_ibfk_1` (`id_currentQuestion`),
   ADD KEY `ownerID` (`ownerID`),
-  ADD KEY `courseID` (`courseID`);
+  ADD KEY `courseID` (`courseID`),
+  ADD KEY `id_currentQuestion` (`id_currentQuestion`);
 
 --
 -- Index pour la table `sessions`
@@ -321,49 +329,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `poll`
 --
 ALTER TABLE `poll`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=626;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 
 --
 -- AUTO_INCREMENT pour la table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `setDeQuestion`
 --
 ALTER TABLE `setDeQuestion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pour la table `stats`
 --
 ALTER TABLE `stats`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `statsBloc`
 --
 ALTER TABLE `statsBloc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9065;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
@@ -386,9 +400,9 @@ ALTER TABLE `questions`
 -- Contraintes pour la table `rooms`
 --
 ALTER TABLE `rooms`
-  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`id_currentQuestion`) REFERENCES `questions` (`id`),
   ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`ownerID`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `rooms_ibfk_3` FOREIGN KEY (`courseID`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `rooms_ibfk_3` FOREIGN KEY (`courseID`) REFERENCES `courses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rooms_ibfk_4` FOREIGN KEY (`id_currentQuestion`) REFERENCES `questions` (`id`) ON DELETE SET NULL;
 
 --
 -- Contraintes pour la table `setDeQuestion`
